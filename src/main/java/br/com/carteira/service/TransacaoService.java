14e04bceb2171d2182878cd6d3d847fc8a03ff64
Luiz Eduardo Costa <luiz_costa@hst.com.br>
2021-09-29 21:55:30 -0300

    Fixing bug when adding a new transaction.

--- src/main/java/br/com/carteira/service/TransacaoService.java
+++ src/main/java/br/com/carteira/service/TransacaoService.java
@@ -3,6 +3,8 @@ package br.com.carteira.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.transaction.Transactional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,9 +27,11 @@ public class TransacaoService {
 		return transacoes.stream().map(t -> modelMapper.map(t, TransacaoDTO.class)).collect(Collectors.toList());
 	}
 
+	@Transactional
 	public void cadastrar(TransacaoFormDTO dto) 
 	{
 		Transacao transacao = modelMapper.map(dto, Transacao.class);
+		transacao.setId(null);
 		transacaoRepository.save(transacao);
 	}
 
