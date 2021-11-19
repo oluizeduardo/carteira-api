e5e137f2789119c57baf930f5e31a95b9302c6d0
Luiz Eduardo Costa <luiz_costa@hst.com.br>
2021-11-18 23:06:57 -0300

    Updated Usuario to receive an email account.

--- src/main/java/br/com/carteira/dto/UsuarioFormDTO.java
+++ src/main/java/br/com/carteira/dto/UsuarioFormDTO.java
@@ -1,5 +1,6 @@
 package br.com.carteira.dto;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -22,4 +23,8 @@ public class UsuarioFormDTO {
 	
 	@NotNull
 	private Integer perfilId;
+	
+	@NotBlank
+	@Email
+	private String email;
 }
