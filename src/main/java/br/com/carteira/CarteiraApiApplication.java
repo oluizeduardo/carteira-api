9cf457347e9ad9b5f8ad48ba0ecca783dc4cd393
Luiz Eduardo Costa <luiz_costa@hst.com.br>
2021-11-19 23:36:16 -0300

    Added READ.me Java front image.

--- src/main/java/br/com/carteira/CarteiraApiApplication.java
+++ src/main/java/br/com/carteira/CarteiraApiApplication.java
@@ -2,8 +2,11 @@ package br.com.carteira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+//Habilita a execução de métodos com @Async.
+@EnableAsync
 public class CarteiraApiApplication {
 
 	public static void main(String[] args) {
